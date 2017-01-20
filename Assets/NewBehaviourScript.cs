d89bec6d235ec22a6f380b24dd33f6bb4b6daf57
GSTEPS\Hanokh.Aloni <hanokhaloni@gmail.com>
2017-01-20 18:20:42 +0200

    Added timer and external reference

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewBehaviourScript : MonoBehaviour {
+
+    public Text test;
+    // Use this for initialization
+    void Start()
+    {
+        test.GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //test.text = GameObject.Find("Slider").GetComponent<Slider>().value;
+
+    }
+
+    public void SetValue(float newValue)
+    {
+        test.text = newValue.ToString();
+    }
+}
